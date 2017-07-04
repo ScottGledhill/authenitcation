e522c5650be08a2f07d12ad9e814c719b3f855d8
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-04 17:02:45 +0100

    payload error corrected

--- routes/users.js
+++ routes/users.js
@@ -54,8 +54,8 @@ router.post('/authenticate', (req, res, next) => {
   });
 });
 
-router.get('/profile', (req, res, next) => {
-  res.send('profile test');
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
 });
 
 module.exports = router;
