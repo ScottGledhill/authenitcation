7a12488d81ce84bd3eda028400f4507d19cfd745
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-04 15:57:08 +0100

    added success and failure for adding a user into post method/user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 router.post('/register', (req, res, next) => {
-  res.send('Register test');
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.email,
+    password: req.body.password
+  })
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({success: false, msg:'failed to register user'});
+    } else {
+      res.json({success: true, msg:'user registered'});
+    }
+  }
 });
 
 router.post('/authenticate', (req, res, next) => {
