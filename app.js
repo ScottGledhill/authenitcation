8cd909791555944cb504d7941788d6597e33e1f5
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-04 14:45:22 +0100

    routes folder created, app.use setup

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
+
+const users = require('./routes/users');
 
 const port = 3000;
 
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
 app.get('/', (req, res) => {
-  res.send('HOMEPAGE')
-})
+  res.send('HOMEPAGE');
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
