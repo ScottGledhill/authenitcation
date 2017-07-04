149c550548a06374fea69be3a5d447243b4918ee
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-04 14:37:59 +0100

    root to homepage added

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express()
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('HOMEPAGE')
+})
+
 app.listen(port, () => {
-  console.log(`Server started on port + ${port}`)
+  console.log(`Server started on port ${port}`)
 });
