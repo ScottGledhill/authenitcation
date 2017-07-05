83103da398425c64ec55031fddc42dcc28cd7851
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-05 11:22:05 +0100

    routes added, bootstrap class added

--- angular-cli/src/app/app.module.ts
+++ angular-cli/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +11,14 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'dashboard', component: DashboardComponent },
+  {path: 'profile', component: ProfileComponent }
+]
 
 @NgModule({
   declarations: [
@@ -24,7 +33,8 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
