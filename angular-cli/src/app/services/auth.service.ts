8d482ebdd61f571a7c402b17cc8385679bc0ca19
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-06 16:57:38 +0100

    auth guard set up, to check logged in

--- angular-cli/src/app/services/auth.service.ts
+++ angular-cli/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {tokenNotExpired} from 'angular2-jwt'
 
 
 @Injectable()
@@ -43,6 +44,11 @@ export class AuthService {
     const token = localStorage.getItem('id_token');
     this.authToken = token;
   }
+
+  loggedIn() {
+    return tokenNotExpired();
+  }
+    
   logout() {
     this.authToken = null;
     this.user = null;
