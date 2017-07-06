7a08f48d2746d17c916d82c88c90eb65f82e08cb
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-07-06 17:05:31 +0100

    dashboard updated, authguard completed

--- angular-cli/src/app/app.module.ts
+++ angular-cli/src/app/app.module.ts
@@ -11,16 +11,17 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
-import { FlashMessagesModule} from 'angular2-flash-messages'
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard'
 
-import {ValidateService} from './services/validate.service';
-import {AuthService} from './services/auth.service';
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'register', component: RegisterComponent },
   {path: 'login', component: LoginComponent },
-  {path: 'dashboard', component: DashboardComponent },
+  {path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] },
   {path: 'profile', component: ProfileComponent }
 ]
 
@@ -41,7 +42,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
